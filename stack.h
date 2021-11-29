4cae5ffec464af8f3d6499f2e81d1b41bd8bc99c
rhythm6 <1452171902@qq.com>
2021-11-29 17:50:22 +0800

    栈1

--- stack.h
+++ stack.h
@@ -1,8 +1,27 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #pragma
+#include<assert.h>
 #include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
 typedef int STDataType;
-struct Stack
+//静态的东西 缺陷太大一般不建议写 
+typedef struct Stack
 {
-	STDataType* a;
-};
+	STDataType* a;//数组实现  
+	int top;//栈顶 
+	int capacity;//容量 
+}ST;
+//把顺序表的表尾当作栈顶 
+//初始化 
+void StackInit(ST* ps);
+//销毁 
+void StackDestory(ST* ps);
+//压栈  把新元素插到栈顶的(上面) 
+void StackPush(ST* ps, STDataType x);
+//出栈  把栈顶元素删除掉 使其下面的元素成为新的栈顶元素 
+void StackPop(ST* ps);
+//计算栈大小 
+void StackSize(STps);
+//判断栈空 
+bool StackEmpty(STps);
--- stacktest.c
+++ stacktest.c
@@ -1,2 +1,10 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include"stack.h"
+int main()
+{
+	ST st;//初始化结构体成员
+	StackInit(&st);//传递st的地址才能改变st 
+
+	StackDestory(&st);//用完销毁 
+	return 0;
+}
